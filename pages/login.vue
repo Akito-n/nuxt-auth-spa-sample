8e67f966105e20ca65fbcd00609bb29e01b6bac6
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-08 18:57:19 +0900

    popup表示

---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="show-auth"></div>
+</template>
+
+<script>
+  export default {
+    setup() {},
+    mounted() {
+      this.$auth0.showLock('show-auth')
+    },
+  }
+</script>
